bb088b06584d3fadd47e1a7580086267de2cca8c
Lambo-Finance <90825587+secretemate@users.noreply.github.com>
2022-12-05 17:03:21 +0200

    push

---
+++ dist/widgets/WalletModal/config.js
@@ -0,0 +1,27 @@
+import Metamask from "./icons/Metamask";
+import MathWallet from "./icons/MathWallet";
+import TokenPocket from "./icons/TokenPocket";
+import TrustWallet from "./icons/TrustWallet";
+import WalletConnect from "./icons/WalletConnect";
+import BinanceChain from "./icons/BinanceChain";
+import SafePalWallet from "./icons/SafePalWallet";
+import { ConnectorNames } from "./types";
+var connectors = [
+    {
+        title: "Metamask",
+        icon: Metamask,
+        connectorId: ConnectorNames.Injected,
+    },
+    {
+        title: "TrustWallet",
+        icon: TrustWallet,
+        connectorId: ConnectorNames.Injected,
+    },
+    {
+        title: "WalletConnect",
+        icon: WalletConnect,
+        connectorId: ConnectorNames.WalletConnect,
+    },
+];
+export default connectors;
+export var connectorLocalStorageKey = "connectorId";
